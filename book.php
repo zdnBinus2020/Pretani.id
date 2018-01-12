c2df0361f83eed20013f8822c077e258ad3d5f7e
zaidan <you@example.com>
2018-01-12 07:24:08 +0700

    initing

---
+++ book.php
@@ -0,0 +1,32 @@
+<?php 
+		header("");
+
+//Server: sql12.freemysqlhosting.net
+//Name: sql12214602
+//Username: sql12214602
+//Password: c7FkFkE4TZ
+//Port number: 3306
+			
+
+$host       =   "sql12.freemysqlhosting.net";
+$user       =   "sql12214602";
+$password   =   "c7FkFkE4TZ";
+$database   =   "sql12214602";
+
+
+$link = mysqli_connect($host, $user, $password  ,$database);
+
+$query_result = $link->query("SELECT * FROM BlueJack")
+	$books = [];
+
+	while($data = $query_result->fetch_assoc() )
+	{
+
+		$books[] =$data;
+	}
+
+	header('content-type: application/json');
+	print json_encode($books);
+
+	
+ ?>
